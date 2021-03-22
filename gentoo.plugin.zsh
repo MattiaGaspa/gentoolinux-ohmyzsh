05db6f5b67f192a4bfd867d7a1489eca94eb34d4
gasmat <53443116+MattiaG-afk@users.noreply.github.com>
2021-03-22 14:55:24 +0100

    Update gentoo.plugin.zsh

--- gentoo.plugin.zsh
+++ gentoo.plugin.zsh
@@ -1,4 +1,9 @@
 if (( $+commands[emerge] )); then
+    alias install='sudo emerge '
+    alias remove='sudo emerge -C '
+    alias search='emerge -s '
+    alias lbin='sudo emerge -K '
+    alias rbin='sudo emerge -G '
     alias sync='sudo emerge --sync'
     alias upgrade='sudo emerge -aDuN world'
     alias clean='sudo emerge --depclean'
@@ -7,4 +12,10 @@ fi
 if (( $+commands[eclean] )); then
     alias cleandist='sudo eclean --deep distfiles'
     alias cleanpkg='sudo eclean-pkg'
+fi
+
+if (( $+commands[euse] )); then
+    alias nuse='sudo euse -E '
+    alias duse='sudo euse -D '
+
 fi
