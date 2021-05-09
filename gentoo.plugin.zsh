c1742a8b218a6cf0022a03582a483a753d888427
gasmat <53443116+MattiaG-afk@users.noreply.github.com>
2021-05-09 11:26:00 +0200

    Update gentoo.plugin.zsh

--- gentoo.plugin.zsh
+++ gentoo.plugin.zsh
@@ -4,14 +4,35 @@ if (( $+commands[emerge] )); then
     alias search='emerge -s '
     alias lbin='sudo emerge -K '
     alias rbin='sudo emerge -G '
-    alias sync='sudo emerge --sync'
-    alias upgrade='sudo emerge -aDuN world'
-    alias clean='sudo emerge --depclean'
+    alias sync='sudo emerge --sync '
+    alias upgrade='sudo emerge -aDuN world '
+    alias clean='sudo emerge --depclean '
 fi
 
+if (( $+commands[genlop] )); then
+    alias ghistroy='sudo genlop -l '
+    alias eta='sudo genlop -c '
+    alias weta='watch -ct -n 1 sudo genlop -c '
+    alias ginfo='sudo genlop -i '
+    alias guhistory='sudo genlop -u '
+    alias estimate='sudo genlop -t '
+
+if (( $+commands[qlop] )); then
+    alias summary='sudo qlop -c '
+    alias time='sudo qlop -t '
+    alias average='sudo qlop -a '
+    alias human='sudo qlop -H '
+    alias machine='sudo genlop -M '
+    alias qmhistory='sudo qlop -m '
+    alias quhistory='sudo qlop -u '
+    alias qahistory='sudo qlop -U '
+    alias qshistory='sudo qlop -s '
+    alias endtime='sudo qlop -e '
+    alias running='sudo qlop -r '
+
 if (( $+commands[eclean] )); then
-    alias cleandist='sudo eclean --deep distfiles'
-    alias cleanpkg='sudo eclean-pkg'
+    alias cleandist='sudo eclean --deep distfiles '
+    alias cleanpkg='sudo eclean-pkg '
 fi
 
 if (( $+commands[euse] )); then
@@ -20,8 +41,8 @@ if (( $+commands[euse] )); then
 fi
 
 if (( $+commands[vim] )); then
-    alias make='sudo vim /etc/portage/make.conf'
-    alias mask='sudo vim /etc/portage/package.mask'
-    alias use='sudo vim /etc/portage/package.use'
-    alias repos='sudo vim /etc/portage/repos.conf'
+    alias make='sudo vim /etc/portage/make.conf '
+    alias mask='sudo vim /etc/portage/package.mask '
+    alias use='sudo vim /etc/portage/package.use '
+    alias repos='sudo vim /etc/portage/repos.conf '
 fi
