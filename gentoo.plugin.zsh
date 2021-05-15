447bb79909b6002198c795cb3cac2b0ae489f5d3
gasmat <53443116+MattiaG-afk@users.noreply.github.com>
2021-05-15 10:55:09 +0200

    Update gentoo.plugin.zsh

--- gentoo.plugin.zsh
+++ gentoo.plugin.zsh
@@ -1,48 +1,51 @@
-if (( $+commands[emerge] )); then
-    alias install='sudo emerge '
-    alias remove='sudo emerge -C '
-    alias search='emerge -s '
-    alias lbin='sudo emerge -K '
-    alias rbin='sudo emerge -G '
-    alias sync='sudo emerge --sync '
-    alias upgrade='sudo emerge -aDuN world '
-    alias clean='sudo emerge --depclean '
-fi
+# EMERGE
+alias emin='sudo emerge '
+alias eminsl='sudo emerge -K '
+alias eminsr='sudo emerge -G '
+alias emre='sudo emerge -C '
+alias emsearch='emerge -s '
+alias emsync='sudo emerge --sync '
+alias emup='sudo emerge -aDuN world '
+alias emclean='sudo emerge --depclean '
 
-if (( $+commands[genlop] )); then
-    alias ghistroy='sudo genlop -l '
-    alias eta='sudo genlop -c '
-    alias weta='watch -ct -n 1 sudo genlop -c '
-    alias ginfo='sudo genlop -i '
-    alias guhistory='sudo genlop -u '
-    alias estimate='sudo genlop -t '
+# PORTAGEQ
+alias pocolor='portageq colormap '
+alias podist='portageq distdir '
+alias povar='portageq envvar '
+alias pomirror='portageq gentoo_mirrors'
+alias poorphan='portageq --orphaned '
 
-if (( $+commands[qlop] )); then
-    alias summary='sudo qlop -c '
-    alias time='sudo qlop -t '
-    alias average='sudo qlop -a '
-    alias human='sudo qlop -H '
-    alias machine='sudo qlop -M '
-    alias qmhistory='sudo qlop -m '
-    alias quhistory='sudo qlop -u '
-    alias qahistory='sudo qlop -U '
-    alias qshistory='sudo qlop -s '
-    alias endtime='sudo qlop -e '
-    alias running='sudo qlop -r '
+# GENLOP
+alias genstroy='sudo genlop -l '
+alias geneta='sudo genlop -c '
+alias genweta='watch -ct -n 1 sudo genlop -c '
+alias geninfo='sudo genlop -i '
+alias genustory='sudo genlop -u '
+alias genstorytime='sudo genlop -t '
 
-if (( $+commands[eclean] )); then
-    alias cleandist='sudo eclean --deep distfiles '
-    alias cleanpkg='sudo eclean-pkg '
-fi
+# QLOP
+alias qsummary='sudo qlop -c '
+alias qtime='sudo qlop -t '
+alias qavg='sudo qlop -a '
+alias qhum='sudo qlop -H '
+alias qmachine='sudo qlop -M '
+alias qmstory='sudo qlop -m '
+alias qustory='sudo qlop -u '
+alias qastory='sudo qlop -U '
+alias qsstory='sudo qlop -s '
+alias qend='sudo qlop -e '
+alias qrun='sudo qlop -r '
 
-if (( $+commands[euse] )); then
-    alias nuse='sudo euse -E '
-    alias duse='sudo euse -D '
-fi
+# ECLEAN
+alias distclean='sudo eclean --deep distfiles '
+alias pkgclean='sudo eclean-pkg '
 
-if (( $+commands[vim] )); then
-    alias make='sudo vim /etc/portage/make.conf '
-    alias mask='sudo vim /etc/portage/package.mask '
-    alias use='sudo vim /etc/portage/package.use '
-    alias repos='sudo vim /etc/portage/repos.conf '
-fi
+# EUSE
+alias newuse='sudo euse -E '
+alias deluse='sudo euse -D '
+
+# VIM
+alias make.conf='sudo vim /etc/portage/make.conf '
+alias package.mask='sudo vim /etc/portage/package.mask '
+alias package.use='sudo vim /etc/portage/package.use '
+alias repos.conf='sudo vim /etc/portage/repos.conf '
